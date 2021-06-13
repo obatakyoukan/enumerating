833a779f0c1f4573f5884513404a34b5848fe48a
obatakyoukan <oba-kyoukan@eis.hokudai.ac.jp>
2021-06-14 01:44:03 +0900

    出力情報修復用の情報を付け加えるようのコメント(まだ，頂点情報の復元は完了していない)

--- clique/include/tree_function.hpp
+++ clique/include/tree_function.hpp
@@ -9,8 +9,8 @@ void node::print(){
 }
 */
 
-void tree::dfs(node& a) {
-// a.print();
+void tree::dfs(node& a) {//列挙では，頂点修復用の方法を作る必要があり，
+ // a.print();
  if( a.leaf() ) return;
  std::cout<< "(";
  for(int i = 0 ; i < a.size() ; i++ ){
